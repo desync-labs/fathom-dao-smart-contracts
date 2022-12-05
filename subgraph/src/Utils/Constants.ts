6571bfa46b20965bbe70f29ee6627c97ccb742f3
ssubik <shresthasubik@gmail.com>
2022-12-05 22:41:03 +0545

    lock positions logic added

--- subgraph/src/Utils/Constants.ts
+++ subgraph/src/Utils/Constants.ts
@@ -1,9 +1,9 @@
 import { BigDecimal, BigInt } from "@graphprotocol/graph-ts"
 
 export class Constants{
-    public static STAKING_CONTRACT:string = '0x70f03000879377A52BCbd5280dC98D9356f9478A'
-    public static STAKING_GETTER:string = '0x8B9Bdbd0a506Ed600395107B619CBcdE658D7288'
-    public static VFTHM:string = '0x046fbf910512003a08e170631b9BcDf5235191B6'
+    public static STAKING_CONTRACT:string = '0x70f03000879377A52BCbd5280dC98D9356f9478A' //new contract-> 0x471d48B3451BA381B708aCaa83f2c9298C2bA34D
+    public static STAKING_GETTER:string = '0x8B9Bdbd0a506Ed600395107B619CBcdE658D7288' //new contract -> 0x4FAB53ddb960565E380c109AC475C945C829b891
+    public static VFTHM:string = '0x046fbf910512003a08e170631b9BcDf5235191B6' //new contract ->0xA41Cd4fB85Db24cB018B95373cf61E01a753af03
 
     public static WAD:BigInt = BigInt.fromI64(10**18)
     public static RAY:BigInt = BigInt.fromI64( 10**27)
@@ -21,3 +21,5 @@ export class Constants{
         return number.div(Constants.WAD).div(Constants.WAD).div(BigInt.fromI64(10**9))
     }
 }
+
+//{"vFTHM":"0xA41Cd4fB85Db24cB018B95373cf61E01a753af03","timelockController":"0x991Abc3fE4881b1e4E73e2Cf83Fa89A06C27dEd2","multiSigWallet":"0x6724E244c9C2549a7ce7B320533B902d1483CfFf","fthmToken":"0x51DA06Bd93b2d9425c19bC64BEBaC19f302CDa53","fthmGovernor":"0x3C94e64a278dFCb334E2264c9ea4b9F4Af7Ea433","stakingImplementation":"0x70492fEF1ED4a5612851752fcf60B92B3fA02b6E","vaultImplementation":"0x1612BCA52Fa4EDEC1ff0fF95561eca15aD73A05f","rewardsCalculator":"0x7195B22A604207605854DC3F874Dec14bb5Cc87f","stakingProxyAdmin":"0xb392432100d7510e79FE2D2E768D948f76D19E78","staking":"0x471d48B3451BA381B708aCaa83f2c9298C2bA34D","vaultProxyAdmin":"0xb843061D00dE9b2c206f4ae0D3297140cb5aaC0D","vault":"0x5CEcF706b5d3e59e0Bc38485Cce32FE5d8f90a52","stakingGetter":"0x4FAB53ddb960565E380c109AC475C945C829b891"}
--- subgraph/src/staking.ts
+++ subgraph/src/staking.ts
@@ -1,10 +1,15 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts";
-import { Staked, Unstaked, StakingPackage } from "../generated/StakingPackage/StakingPackage"
-import { StakedEvent, UnstakedEvent, Staker, ProtocolStat} from "../generated/schema";
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
+import { Staked, Unstaked, StakingPackage, Pending } from "../generated/StakingPackage/StakingPackage"
+import { StakedEvent, UnstakedEvent, Staker, ProtocolStat, LockPosition} from "../generated/schema";
 import { StakingGetter } from "../generated/StakingPackage/StakingGetter"
 import { ERC20 } from "../generated/StakingPackage/ERC20"
 import { Constants } from "./Utils/Constants"
-
+//TODO Still:
+//APR -Subik -Easy
+//Daily Rewards - Subik - Easy
+//Get Fees - Subik // Can we call? - Tricky. Ask Zach
+//Get Lock Period - Subik// Need to add to event lock period -> Easy -> Add event
+//Use new contracts so that events can be handled in a way that aggregates the values
 export function stakeHandler(event: Staked): void {
     // load ProtocolStat (create if first stake event)
     let protocolStats = ProtocolStat.load(Constants.STAKING_CONTRACT)
@@ -27,6 +32,16 @@ export function stakeHandler(event: Staked): void {
     stakedEvent.blockNumber = event.block.number
     stakedEvent.blockTimestamp = event.block.timestamp
     stakedEvent.transaction = event.transaction.hash
+
+    let lockPosition = new LockPosition(event.params.account)
+    lockPosition.account = event.params.account
+    lockPosition.streamShares = event.params.streamShares
+    lockPosition.nVoteToken = event.params.nVEMainTkn
+    //TODO Add amount
+    lockPosition.lockId = event.params.lockId
+    lockPosition.blockNumber = event.block.number
+    lockPosition.blockTimestamp = event.block.timestamp
+    lockPosition.transaction = event.transaction.hash
    
     // Update staker (create staker if first stake for account)
     let staker = Staker.load(event.params.account.toHexString())
@@ -94,6 +109,91 @@ export function unstakeHandler(event: Unstaked): void {
         unstakedEvent.blockTimestamp = event.block.timestamp
         unstakedEvent.transaction = event.transaction.hash
         unstakedEvent.save()
+        completeUnstake(event.params.account,event.params.lockId)
+    }
+    
+}
+
+export function partialUnstakeHandler(event: Unstaked): void {
+    // define contracts
+    let stakingPackage = StakingPackage.bind(Address.fromString(Constants.STAKING_CONTRACT))
+    let stakingGetter = StakingGetter.bind(Address.fromString(Constants.STAKING_GETTER))
+    let vfthmToken = ERC20.bind(Address.fromString(Constants.VFTHM))
+
+    // update staker data
+    let staker = Staker.load(event.params.account.toHexString())
+    if (staker != null) {
+        // call contract to set TOTAL STAKED FOR USER
+        staker.totalStaked = stakingGetter.getUserTotalDeposit(event.params.account)
+        // call VFTHM contract to get balance for user
+        staker.accruedVotes = vfthmToken.balanceOf(event.params.account)
+    }
+
+    // update protocol stats
+    let protocolStats = ProtocolStat.load(Constants.STAKING_CONTRACT)
+    if (protocolStats != null) {
+        // call contract to set TOTAL STAKED FOR PROTOCOL 
+        protocolStats.totalStakeFTHM = stakingPackage.totalAmountOfStakedMAINTkn();
+        protocolStats.totalVotes = stakingPackage.totalAmountOfveMAINTkn();
+        protocolStats.totalUnstakeEvents = protocolStats.totalUnstakeEvents.plus(BigInt.fromString('1'))
+        protocolStats.save()
+
+        // store UnstakedEvent data
+        let unstakedEvent = new UnstakedEvent(protocolStats.totalUnstakeEvents.toString())
+        unstakedEvent.account = event.params.account
+        unstakedEvent.amount = event.params.amount
+        unstakedEvent.lockId = event.params.lockId
+        unstakedEvent.blockNumber = event.block.number
+        unstakedEvent.blockTimestamp = event.block.timestamp
+        unstakedEvent.transaction = event.transaction.hash
+        unstakedEvent.save()
+        partialUnstakeLockPosition(event.params.account,event.params.lockId, event.params.amount)
+    }
+    
+}
+export function pendingHandler(event: Pending): void {
+   // Pending(uint256 indexed streamId, address indexed account, uint256 indexed pendings);
+   let staker = Staker.load(event.params.account.toHexString())
+   if (staker != null){
+        staker.claimedAmount = event.params.pendings
+   }
+}
+
+
+function completeUnstake(account: Bytes, lockId: BigInt): void{
+    let staker = Staker.load(account.toHexString())
+    if (staker != null) {
+        let lengthOfLockPositions = staker.lockPositions.length
+        if(lengthOfLockPositions>0){
+            let lastLockPosition = staker.lockPositions[lengthOfLockPositions - 1]
+            //TODO: Check this again
+            let lockIdInt = lockId.toI32();
+            staker.lockPositions[lockIdInt - 1] = lastLockPosition
+            staker.lockPositions.pop()
+            staker.save()
+        }
     }
 }
+
+function partialUnstakeLockPosition(account: Bytes, lockId: BigInt, amount: BigInt):void{
+    let staker = Staker.load(account.toHexString())
+    if (staker != null) {
+        let lockIdInt = lockId.toI32();
+        let unstakedLockPosition = staker.lockPositions[lockIdInt - 1]
+        //TODO Subtract amount from lockposition.amount
+        
+    }
+}
+
+function getAPR(): void{
+    
+}
+
+function getDailyRewards(): void {
+    
+}
+
+
+
+
 
