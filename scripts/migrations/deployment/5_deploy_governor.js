220503428c8e699c0b15e6fec40d644cee09882e
maaxnaax <maxhayeshome@gmail.com>
2022-12-12 11:58:51 +0400

    Updated Governance requirements

--- scripts/migrations/deployment/5_deploy_governor.js
+++ scripts/migrations/deployment/5_deploy_governor.js
@@ -6,8 +6,8 @@ const MultiSigWallet = artifacts.require("./dao/treasury/MultiSigWallet.sol");
 const VMainToken_address = VMainToken.address;
 const TimelockController_address = TimelockController.address;
 const MultiSigWallet_address = MultiSigWallet.address;
-const initialVotingDelay = 43200; 
-const votingPeriod = 43200 * 5; 
+const initialVotingDelay = 30; 
+const votingPeriod = 450; 
 const initialProposalThreshold = 1000;
 
 
