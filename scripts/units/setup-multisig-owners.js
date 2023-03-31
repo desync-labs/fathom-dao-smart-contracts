43d7b6a784cd1c609c1625b0cd4a786e1681f6af
ssubik <shresthasubik@gmail.com>
2023-03-31 12:23:39 +0545

    modifying few scripts

--- scripts/units/setup-multisig-owners.js
+++ scripts/units/setup-multisig-owners.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const constants = require('./helpers/constants')
 const txnHelper = require('./helpers/submitAndExecuteTransaction')
 
+const rawdata = fs.readFileSync(constants.PATH_TO_ADDRESSES);
+const addresses = JSON.parse(rawdata);
+
 const COUNCIL_1_PLACEHOLDER = "0xc0Ee98ac1a44B56fbe2669A3B3C006DEB6fDd0f9";
 const COUNCIL_2_PLACEHOLDER = "0x01d2D3da7a42F64e7Dc6Ae405F169836556adC86";
 
@@ -22,10 +25,10 @@ const _encodeAddOwnersFunction = (_accounts) => {
 }
 
 module.exports = async function(deployer) {
-    
+
     await txnHelper.submitAndExecute(
         _encodeAddOwnersFunction([COUNCIL_1_PLACEHOLDER, COUNCIL_2_PLACEHOLDER]),
-        MULTISIG_WALLET_ADDRESS,
+        addresses.multiSigWallet,
         "setupMultisigOwner"
     )
 
