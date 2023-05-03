e31bd347aaf36051eb284985e720567177320001
ssubik <shresthasubik@gmail.com>
2023-05-03 13:26:54 +0545

    working on scripts

--- scripts/units/stablecoin/collateral-pool-config/set-debt-ceiling.js
+++ scripts/units/stablecoin/collateral-pool-config/set-debt-ceiling.js
@@ -3,8 +3,8 @@ const txnHelper = require('../../helpers/submitAndExecuteTransaction')
 
 const constants = require('../../helpers/constants')
 const addressesConfig = require(constants.PATH_TO_ADDRESSES_FOR_STABLECOIN_FOLDER)
-const COLLATERAL_POOL_ID = ''
-const DEBT_CEILING = ''
+const COLLATERAL_POOL_ID = '0x5844430000000000000000000000000000000000000000000000000000000000'
+const DEBT_CEILING = web3.utils.toWei('3999999999999999986441897348723255952923416883888128','wei')
 const COLLATERAL_POOL_CONFIG_ADDRESS =addressesConfig.COLLATERAL_POOL_CONFIG_ADDRESS
 const _encodeSetDebtCeiling = (_collateralPoolId, _debtCeiling) => {
     let toRet =  web3.eth.abi.encodeFunctionCall({
